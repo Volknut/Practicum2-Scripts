a3dd5b9aef10327e9d20be4e540cd3bb34b2c25d
Valknut <138918153+Volknut@users.noreply.github.com>
2023-07-21 13:46:45 -0500

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -2,7 +2,7 @@
 x=10               #NOT x   =  10 no spaces
 X=20               #variables are case sensitive
 y=                    #NULL variable
-echo "x = $x"
-echo "X = $X"
-echo "y = $y"
+echo "x = $x" # this is telling you the value of x which is 10
+echo "X = $X" # this is telling you the value of X which is 20
+echo "y = $y" # this is telling you the value of y
 
