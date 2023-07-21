296efa8cafded77a8e10f6196152103d31a8c095
Valknut <138918153+Volknut@users.noreply.github.com>
2023-07-21 13:43:36 -0500

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # example of using arguments to a script
-echo "My first name is $1"
-echo "My surname is $2"
-echo "Total number of arguments is $#"
+echo "My first name is $1" # this is setting first name 1
+echo "My surname is $2" # this is statinf last name of 2
+echo "Total number of arguments is $#" # this is telling you the total numer of arguments
 
